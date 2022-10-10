1f3131fdcf26edc37bc141cb2f43e7c0af32ac9f
Vietp3991 <73873363+Vietp3991@users.noreply.github.com>
2022-10-10 16:25:23 +0700

    cv

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// menu
+const menu = document.querySelector(".navbar__links");
+const menuButton = document.querySelector(".navbar__icons");
+
+menuButton.addEventListener("click", ()=>{
+    menu.classList.toggle("navbar__open");
+    menuButton.classList.toggle("open");
+})
+
